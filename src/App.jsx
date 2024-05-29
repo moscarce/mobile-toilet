dc440230f95396e4667f47570458a9a61788cd83
mscarce <oluwa14tobi@gmail.com>
2024-05-29 08:49:43 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <>
+      
+      <p className="text-6xl font-bold">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
